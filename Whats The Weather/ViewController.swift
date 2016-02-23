d8fe3f99aa2c6a5fb06b1435e5299f41ecd27738
Thavish Perera <thavish.perera@gmail.com>
2016-02-22 22:29:26 -0500

    Added faster error message for empty text field

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -66,52 +66,21 @@ class ViewController: UIViewController {
         
         let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
         
-        if let url = attemptedUrl {
-        
-            let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
-                
-                if let urlContent = data{
-                    
-                    let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-                    
-                    let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-                    
-                    if websiteArray.count > 1 {
-                        
-                        let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
-                        
-                        if weatherArray.count > 1 {
-                            
-                            wasSuccessful = true
-                            
-                            let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
-                            
-                            dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                                self.resultLabel.text = weatherSummary
-                            })
-                            
-                            
-                        }
-                        
-                    }
-                    
-                    if wasSuccessful == false {
-                        self.resultLabel.textColor = UIColor.redColor()
-                        self.resultLabel.text = "Couldn't find the weather for that city - please try again"
-                    }
-
-                    
-                    
-                }
-                
-            } //End of task
+        if cityTextField.text != "" {
             
-            task.resume()
+        
+            if let url = attemptedUrl {
+                
+                runTask(url)
+                
+            } else {
+                resultLabel.textColor = UIColor.redColor()
+                resultLabel.text = "Couldn't find the weather for that city - please try again"
+            }
         } else {
             resultLabel.textColor = UIColor.redColor()
-            resultLabel.text = "Couldn't find the weather for that city - please try again"
+            resultLabel.text = "Please enter a city"
         }
-
     } // End of Action
     
     
