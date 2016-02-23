998c0593a4af5e99cdee50c4c5385e02b28192e9
Thavish Perera <thavish.perera@gmail.com>
2016-02-22 22:28:44 -0500

    Put task in separate function

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -13,12 +13,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var cityTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
     
+    func runTask(url: NSURL) {
+        
+        var wasSuccessful = false
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            if let urlContent = data{
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                if websiteArray.count > 1 {
+                    
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 1 {
+                        
+                        wasSuccessful = true
+                        
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            self.resultLabel.text = weatherSummary
+                        })
+                        
+                        
+                    }
+                    
+                }
+                
+                if wasSuccessful == false {
+                    self.resultLabel.textColor = UIColor.redColor()
+                    self.resultLabel.text = "Couldn't find the weather for that city - please try again"
+                }
+                
+                
+                
+            }
+            
+        } //End of task
+
+        task.resume()
+        
+    } //End of func
+    
     @IBAction func findWeather(sender: AnyObject) {
         
         resultLabel.textColor = UIColor.whiteColor()
         resultLabel.text = "Checking..."
-        
-        var wasSuccessful = false
         
         let attemptedUrl = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
         
